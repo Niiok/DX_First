576d1a538adc14900ad14dc96ab0623573b27fb1
Niiok <play.niiok@gmail.com>
2021-10-20 10:41:35 +0900

    device manager

---
+++ 04_DX3D9/04_DX3D9/cMainGame.h
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class cMainGame
+{
+public :
+	cMainGame();
+	~cMainGame();
+
+public:
+	void Setup();
+	void Update();
+	void Render();
+};
+
---
+++ 04_DX3D9/04_DX3D9/framework.h
@@ -0,0 +1,48 @@
+﻿// header.h: 표준 시스템 포함 파일
+// 또는 프로젝트 특정 포함 파일이 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#define _USE_MATH_DEFINES
+#define EIGEN_DONT_VECTORIZE
+#define EIGEN_DISABLE_UNALIGNED_ARRAY_ASSERT
+#include <vector>
+#include <math.h>
+
+#include <d3dx9.h>
+#pragma comment(lib, "d3d9.lib")
+#pragma comment(lib, "d3dx9.lib")
+
+#include "Eigen/Eigen"
+#include "EigenDX.h"
+
+using namespace std;
+using namespace Eigen;
+
+extern HWND g_hWnd;
+#define PI M_PI
+
+#define SAFE_RELEASE(p) {if(p) p->Release(); p = NULL;}
+#define SINGLETONE(class_name) \
+	private: \
+		class_name(void); \
+		~class_name(void); \
+	public: \
+		static class_name* GetInstance() \
+		{ \
+			static class_name instance; \
+			return &instance; \
+		}
+
+#include "cDeviceManager.h"
