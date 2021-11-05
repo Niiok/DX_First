a63b96daa74ec8dbeba55552901b7118771ce472
Niiok <play.niiok@gmail.com>
2021-11-05 23:14:02 +0900

    main

---
+++ DX11/UnitTst/Main.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+#include "Main.h"
+
+
+
+void Main::Initialize()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Initialize();
+	}
+}
+
+void Main::Ready()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Ready();
+	}
+}
+
+void Main::Destroy()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Destroy();
+		SafeDelete(exe);
+	}
+}
+
+void Main::Update()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Update();
+	}
+}
+
+void Main::PreRender()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->PreRender();
+	}
+}
+
+void Main::Render()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->Render();
+	}
+}
+
+void Main::PostRender()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->PostRender();
+	}
+}
+
+void Main::ResizeScreen()
+{
+	for (IExecute* exe : executes)
+	{
+		exe->ResizeScreen();
+	}
+}
+
+void Main::Push(IExecute * execute)
+{
+	executes.push_back(execute);
+}
